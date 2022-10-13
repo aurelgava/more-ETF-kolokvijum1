d49e6d34e5e9859bbaa55674295c297dd1dcdec8
aurel <aurel@aurel-PC>
2022-10-13 13:36:30 +0200

    Flota

---
+++ src/more/Flota.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package more;
+
+/**
+ *
+ * @author aurel
+ */
+public class Flota {
+    private static class Elem {
+        Brod b; Elem sled;
+        Elem(Brod b) { this.b = b; }
+    }
+    
+}
